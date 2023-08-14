58bc50e34c0fc0c1d819469ed4a6e2e4a00ce030
Basil <ohuruogubasil@gmail.com>
2023-08-14 17:23:58 +0100

    added:global-error-handler

--- src/setupServer.ts
+++ src/setupServer.ts
@@ -14,6 +14,8 @@ import Logger from "bunyan";
 import apiStats from "swagger-stats";
 import "express-async-errors";
 import { config } from "./config";
+import applicationRoutes from "./routes";
+import { CustomError, IErrorResponse } from "@global/helpers/error-handler";
 
 const SERVER_PORT = 5000;
 
@@ -61,9 +63,32 @@ export class ChattyServer {
     app.use(urlencoded({ extended: true, limit: "50mb" }));
   }
 
-  private routeMiddleware(app: Application): void {}
+  private routeMiddleware(app: Application): void {
+    applicationRoutes(app);
+  }
 
-  private globalErrorHandler(app: Application): void {}
+  private globalErrorHandler(app: Application): void {
+    app.all("*", (req: Request, res: Response) => {
+      res
+        .status(HTTP_STATUS.NOT_FOUND)
+        .json({ message: `${req.originalUrl} not found` });
+    });
+
+    app.use(
+      (
+        error: IErrorResponse,
+        _req: Request,
+        res: Response,
+        next: NextFunction
+      ) => {
+        console.log(error);
+        if (error instanceof CustomError) {
+          return res.status(error.statusCode).json(error.serializeErrors());
+        }
+        next();
+      }
+    );
+  }
 
   private async startServer(app: Application): Promise<void> {
     // if (!config.JWT_TOKEN) {
